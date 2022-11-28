58a101493b9872e418a78fef8056cb91cbba06eb
kiryl-f <67046881+kiryl-f@users.noreply.github.com>
2022-11-28 12:32:05 +0300

    Update authentication_ajax.js

--- js/authentication_ajax.js
+++ js/authentication_ajax.js
@@ -1,11 +1,11 @@
 $(function () {
-    $("#next").on('submit', function (e) {
+    $('form').on('submit', function (e) {
         e.preventDefault();
         $.ajax({
             type: 'post',
             url: 'check_user.php',
             dataType: 'json',
-            data: $(e.target).serialize(),
+            data: $('form').serialize(),
             success: function (response) {
                 if(response['found'] === 'true') {
                     location.href = 'index.php';
